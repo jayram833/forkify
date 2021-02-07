28bbf70d105e64d94e0eac47608ad028229496c7
jpansare <jaypansare96@gmail.com>
2021-02-07 20:30:18 +0530

    updated

---
+++ src/js/controller.js
@@ -0,0 +1,45 @@
+import * as model from './model.js';
+import recipeView from './views/recipeView.js';
+import searchView from './views/searchView';
+// import icons from '../img/icons.svg'; //parcel 1
+
+const { async } = require('q');
+
+const controlRecipes = async function () {
+  try {
+    const id = window.location.hash.slice(1);
+
+    if (!id) return;
+    recipeView.renderSpinner();
+
+    // Loading recipe
+    await model.loadRecipe(id);
+
+    // Rendering the recipe
+    recipeView.render(model.state.recipe);
+  } catch (err) {
+    recipeView.renderError();
+  }
+};
+
+const controlSearchResults = async function () {
+  try {
+    // 1)Get serach query
+    const query = searchView.getQuery();
+    if (!query) return;
+    
+    //2)Load Search results
+    await model.loadSearchResults(query);
+    // 3)Render results
+    console.log(model.state.search.results);
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+const init = function () {
+  recipeView.addHandlerRender(controlRecipes);
+  searchView.addHandlerSearch(controlSearchResults);
+};
+
+init();
