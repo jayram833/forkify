60bda80536dc524cdde7b0172b6cefa0a2d14018
jpansare <jaypansare96@gmail.com>
2021-02-17 22:56:41 +0530

    update

--- src/js/controller.js
+++ src/js/controller.js
@@ -7,6 +7,9 @@ import paginationView from './views/paginationView.js';
 import bookmarksView from './views/bookmarksView.js';
 import addRecipeView from './views/addRecipeView.js';
 
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
+import { async } from 'regenerator-runtime';
 // import icons from '../img/icons.svg'; //parcel 1
 
 // const { async } = require('q');
